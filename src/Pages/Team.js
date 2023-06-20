cc80ac156df17c3a51908fef89aeeb294d6bf5da
a-blaine <ai.blaine4@protonmail.com>
2023-06-20 20:08:52 +0100

    Created Team page

---
+++ src/Pages/Team.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Team() {
+  return (
+    <div className="Team">
+      <header>
+        <h2>Our Professional</h2>
+        <p>Home/Team</p>
+      </header>
+      <div className="team-card">
+        <img src="/" alt="Team member" />
+        <h4>Charlotte Levi</h4>
+        <p>Design, Australia</p>
+        <div className="team-social-icons"></div>
+      </div>
+    </div>
+  );
+}
