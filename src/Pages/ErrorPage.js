71f5d82d4682b2bfa2ca846191fce2a6d6d7ba3a
a-blaine <ai.blaine4@protonmail.com>
2023-06-20 20:04:39 +0100

    Created Error page

---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div className="Error">
+      <div className="container">
+        <h1>404</h1>
+        <h2>We are sorry, but the page you requested was not found</h2>
+        <button>Back to Home</button>
+      </div>
+    </div>
+  );
+}
