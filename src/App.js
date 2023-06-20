99d28b26deaa97ed9dd91202dcb4efee71fc0995
a-blaine <ai.blaine4@protonmail.com>
2023-06-20 19:59:59 +0100

    Added hero and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className="container">
+        <div className="hero">
+          <h1>Hello world</h1>
+        </div>
+      </div>
+      <footer>
+        This page is coded by Ashley Blaine and is open-sourced on GitHub
+      </footer>
     </div>
   );
 }
