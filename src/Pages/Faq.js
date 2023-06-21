a92ed3382706480d28459d261a5ed4238d310155
a-blaine <ai.blaine4@protonmail.com>
2023-06-21 18:38:31 +0100

    Created FAQ page

---
+++ src/Pages/Faq.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function Faq() {
+  return (
+    <div className="Faq">
+      <header>
+        <h2>FAQs</h2>
+        <p>Home/FAQ</p>
+      </header>
+      <h2>Every Question Answered</h2>
+      <div className="accordion">
+        <div className="question">
+          <h3>What is the Hipcouch Interior Design Service?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>So, how exactly does this work?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>What cities do you currently operate in?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>Hipcouch Interior Design Service?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>What kind of interior designer do you have?</h3>
+          <p>ANSWER</p>
+        </div>
+      </div>
+      <h2>Project related questions</h2>
+      <div className="accordion">
+        <div className="question">
+          <h3>How long does it take?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>Can I use my existing furnishings?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>I put into redesigning my interior?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>What do your services cost?</h3>
+          <p>ANSWER</p>
+        </div>
+        <div className="question">
+          <h3>Do you offer free consultations?</h3>
+          <p>ANSWER</p>
+        </div>
+      </div>
+    </div>
+  );
+}
