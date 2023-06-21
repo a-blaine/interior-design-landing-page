9fda27e57e9be05c37039aeb7867d67a8b6fd8cb
a-blaine <ai.blaine4@protonmail.com>
2023-06-21 18:45:43 +0100

    Created ProjectDetails structure

---
+++ src/Pages/ProjectDetails.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function ProjectDetails() {
+  return (
+    <div className="ProjectDetails">
+      <div className="client-details">
+        <ul>
+          <li>
+            Client <span>Your client name</span>
+          </li>
+          <li>
+            Category <span>Interiors</span>
+          </li>
+          <li>
+            Tags <span>itnerior, Home</span>
+          </li>
+          <li>
+            Date <span>Date 23, 02, 2022</span>
+          </li>
+          <li>
+            Link <span>Link example.com</span>
+          </li>
+        </ul>
+      </div>
+      <h2>Minimal Look Bedrooms</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <br />
+      <p>
+        Turpis egestas integer eget aliquet nibh praesent tristique magna.
+        Scelerisque felis imperdiet proin fermentum leo vel. Rhoncus aenean vel
+        elit scelerisque. Natoque penatibus et magnis dis parturient montes
+        nascetur ridiculus. Mi tempus imperdiet nulla malesuada pellentesque
+        elit. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros
+        in. Ullamcorper a lacus vestibulum sed arcu. Pellentesque sit amet
+        porttitor eget dolor morbi non arcu. Sit amet aliquam id diam maecenas
+        ultricies mi. Interdum velit euismod in pellentesque massa placerat.
+      </p>
+      <img src="/" alt="Interior design bedroom" />
+    </div>
+  );
+}
