abb1b5810eb71ec08e3a95b835f064deac2e325c
a-blaine <ai.blaine4@protonmail.com>
2023-06-23 21:19:24 +0100

    Created and styled Join component

---
+++ src/Components/JoinInterno.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Join() {
+  return (
+    <div className="join-container">
+      <h2>Want to join Interno?</h2>
+      <p>It is a long established fact readers will be distracted.</p>
+      <a href="/" className="button color-btn">
+        Contact Us
+        <span class="color-btn-arrow">
+          <img src="./assets/Icons/ArrowUpRight.png" alt="Arrow" />
+        </span>
+      </a>
+    </div>
+  );
+}
