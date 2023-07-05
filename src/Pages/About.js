626a2b43ecaf31e9271f4adb325a769ed5be09ef
a-blaine <ai.blaine4@protonmail.com>
2023-07-06 00:20:11 +0100

    Updated About contents and styling

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./About.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTwitter,
@@ -11,15 +12,58 @@ export default function About() {
   return (
     <div className="About">
       <section>
+        <p>
+          I like an interior that defies labeling. I don't really want someone
+          to walk into a room and know that I did it
+        </p>
+        <p>-BUNNY WILLIAMS</p>
+      </section>
+      <section>
+        <div className="row">
+          <div className="col-5">
+            <h3>What We Do</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the amount of readable content of a page when looking at its
+              layouts the points of utility that it has must be more-or-less
+              normal.
+            </p>
+            <a href="/" className="button large-btn default-btn-arrow">
+              Our Concept
+            </a>
+          </div>
+          <div className="col">
+            <img src="../Assets/Images/Hompage2.png" alt="Interior design" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <img src="../Assets/Images/Hompage2.png" alt="Interior design" />
+          </div>
+          <div className="col">
+            {" "}
+            <h3>The End Result</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the amount of readable content of a page when looking at its
+              layouts the points of utility that it has must be more-or-less
+              normal.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
         <h2>What People Think About Us</h2>
         <div className="card-container">
           <div className="card">
-            <div className="card-front"></div>
-            <img src="../assets/Team/Avery.png" alt="People photo" />
-
+            <div className="card-front">
+              <img src="../assets/Team/Avery.png" alt="People photo" />
+            </div>
             <div className="card-back">
-              <h5>Natashe Julie</h5>
-              <p>Design, Australia</p>
+              <div className="card-header">
+                <h5>Natashe Julie</h5>
+                <p>Design, Australia</p>
+              </div>
               <div className="social-icons">
                 <FontAwesomeIcon icon={faFacebook} className="sns-icon" />
 
